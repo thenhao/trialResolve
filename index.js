763da3e34261494aa63ae97969eb8ead007f506d
thenhao <94664145+thenhao@users.noreply.github.com>
2022-03-05 10:37:03 +0800

    updated main

--- index.js
+++ index.js
@@ -16,6 +16,32 @@
 
 // }
 
+/*added things in main here*
+func countingSort(_ array: [Int]) {
+  // Create an array to store the count of each element
+  let maxElement = array.max() ?? 0
+  var countArray = [Int](repeating: 0, count: Int(maxElement + 1))
+  
+  for element in array {
+    countArray[element] += 1
+  }
+  var z = 0
+  var sortedArray = [Int](repeating: 0, count: array.count)
+
+  for index in 1 ..< countArray.count {
+    //print index element required number of times
+    while countArray[index] > 0 {
+      sortedArray[z] = index
+      z += 1
+      countArray[index] -= 1
+    }
+  }
+  
+  print(sortedArray)
+}
+
+*/
+
 // For simplicity, consider the data in the range 0 to 9. 
 // Input data: 1, 4, 1, 2, 7, 5, 2
 //   1) Take a count array to store the count of each unique object.
